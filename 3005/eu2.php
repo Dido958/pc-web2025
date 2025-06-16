f7e388e79b0575dbb3370c97498e2f18a200629c
Diogo <166051802+Dido958@users.noreply.github.com>
2025-06-15 23:25:03 -0300

    Delete 3005 directory

--- 3005/eu2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lines=file("novo_arquivo.txt");
-foreach($lines as $linhas){
-    echo $linhas;
-}
-?>
--- 3005/index.php
+++
@@ -1,7 +0,0 @@
-    <?php
-    $arquivio=fopen("meu arquivo.txt","w");
-    if($arquivio){
-        fwrite($arquivio, "muito tenkiu yo");
-        fclose($arquivio);
-    }
-    ?>
